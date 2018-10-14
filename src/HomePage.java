8200a6fa5bc602fb80923ba3f86b862645c53521
Shabd <shabd.179301181@muj.manipal.edu>
2018-10-14 19:19:47 +0530

    add : create savings bank account

--- src/HomePage.java
+++ src/HomePage.java
@@ -29,6 +29,6 @@ public class HomePage {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-       }
+        }
     }
 }
--- src/SavingsCreateAccount.java
+++ src/SavingsCreateAccount.java
@@ -1,10 +1,16 @@
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
 
@@ -30,7 +36,33 @@ public class SavingsCreateAccount implements Initializable {
             emptyFields.setContentText("Some Fields are missing");
             emptyFields.show();
             return;
+        }
 
+        try {
+            String url = "jdbc:mysql://localhost:3306/bank_user_details?useSSL=false";
+            String user = "root";
+            String pass = "password";
+            Connection sqlConnection = DriverManager.getConnection(url, user, pass);
+            Statement sqlStatement = sqlConnection.createStatement();
+            String query = "insert into user_details(name, dob, address, phone_number, email, aadhar_number) values" +
+                    "(\' " + name.getText() +"\'," +
+                    "\' " + dob.getValue() + "\'," +
+                    "\' " + residentialAddress1.getText() + "," + residentialAddress2.getText() + "," + residentialAddress3.getText() +  "\'," +
+                    "\' " + phoneNumber.getText() + "\'," +
+                    "\' " + emailAddress.getText() + "\'," +
+                    "\' " + aadharNumber.getText() + "\')";
+            sqlStatement.executeUpdate(query);
+
+            Alert confirmation = new Alert(Alert.AlertType.INFORMATION);
+            confirmation.setHeaderText("Confirmation");
+            confirmation.setContentText("Account Created Successfully");
+            confirmation.showAndWait();
+
+            Pane newPane = FXMLLoader.load(getClass().getResource("homePage.fxml"));
+            Main.primaryStage.setScene(new Scene(newPane, 1000, 600));
+            Main.primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
